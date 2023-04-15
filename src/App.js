d238eddff41e035113685458910ecb10dbd43fcd
Davi Lopes <daviassis12@gmail.com>
2023-04-15 14:56:51 -0300

    news

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [news, setNews] = useState([]);
+  
+
+  useEffect(() => {
+    axios.get('https://newsapi.org/v2/top-headlines?country=br&apiKey=db7edc8529f9484389aa240a5e114d16')
+      .then(response => {
+        setNews(response.data.articles);
+        console.log(response.data.articles);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PORTAL DAS NOTÍCIAS</h1>
       </header>
+      <div className="card-container">
+      {news.map(article => {
+  console.log(article.urlToImage); 
+  return (
+    <div className="card" key={article.title}>
+      <img src={article.urlToImage ? article.urlToImage : process.env.PUBLIC_URL + '/news11.png'} alt={article.title} />
+
+      <h2>{article.title}</h2>
+      <p>{article.description}</p>
+      <a href={article.url} target="_blank" rel="noopener noreferrer">Saiba mais</a>
+    </div>
+  );
+})}
+
+</div>
     </div>
   );
 }
 
 export default App;
+
